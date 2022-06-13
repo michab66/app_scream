3d1db39249992b51d5b6717bf1687439f27d8669
Michael Binz <michael.binz@daimler.com>
2022-06-13 11:20:21 +0200

    Added parser test.

--- src/test/java/de/michab/scream/scanner/SchemeParserTest.java
+++ src/test/java/de/michab/scream/scanner/SchemeParserTest.java
@@ -7,6 +7,25 @@
  */
 package de.michab.scream.scanner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
 public class SchemeParserTest
 {
+    @Test
+    public void basic(){
+        try {
+            // Missing closing brace.
+            SchemeParser sp = new SchemeParser( "(+ 300 13" );
+            var x = sp.getExpression();
+            fail();
+        }
+        catch( FrontendX e )
+        {
+            // Unexpected end of input.
+            assertEquals( 37, e.getId() );
+        }
+    }
 }
