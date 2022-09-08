de536d7cea549827fff10f754c7e67088407079a
Michael Binz <michab66@gmail.com>
2022-09-08 21:07:20 +0200

    Added load test.

--- src/test/java/de/michab/scream/SchemeTest.java
+++ src/test/java/de/michab/scream/SchemeTest.java
@@ -4,6 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+
 import javax.script.ScriptException;
 
 import org.junit.jupiter.api.Test;
@@ -53,5 +57,32 @@ public class SchemeTest
         var result = se.eval( "(- 320 7)" );
 
         assertEquals( "313", result.toString() );
+    }
+
+    @Test
+    public void loadTest() throws Exception
+    {
+        var tp = Files.createTempFile( getClass().getSimpleName(), ".tmp" );
+        File tf = tp.toFile();
+        tf.deleteOnExit();
+        try ( var fw = new FileWriter( tf ) )
+        {
+            fw.write(
+                    """
+(define one 1)
+(define two 2)
+                    """);
+        }
+
+        SchemeInterpreter2 si = new SchemeInterpreter2();
+        var se = si.getScriptEngine();
+
+        se.eval( String.format( "(load \"%s\")", tf.toString() ) );
+
+        var one = se.eval( "one" );
+        var two = se.eval( "two" );
+
+        assertEquals( "1", one.toString() );
+        assertEquals( "2", two.toString() );
     }
 }
