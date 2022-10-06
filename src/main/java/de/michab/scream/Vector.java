423725bc42406701091c3615538a9cf63909cc02
Michael Binz <michab66@gmail.com>
2022-10-06 21:41:39 +0200

    vector rework (#23)
    * Implemented.
    
    * Implemented.

--- src/main/java/de/michab/scream/Vector.java
+++ src/main/java/de/michab/scream/Vector.java
@@ -1,15 +1,13 @@
-/* $Id: Vector.java 190 2009-07-01 20:53:40Z Michael $
+/*
+ * Scream @ https://github.com/michab/dev_smack
  *
- * Scream / Kernel
- *
- * Released under Gnu Public License
- * Copyright (c) 1998-2009 Michael G. Binz
+ * Copyright © 1998-2022 Michael G. Binz
  */
 package de.michab.scream;
 
 import java.util.Arrays;
 
-
+import de.michab.scream.ScreamException.Code;
 
 /**
  * Vectors are heterogeneous structures whose elements are indexed by integers.
@@ -23,227 +21,210 @@ import java.util.Arrays;
  * one less than the length of the vector.
  */
 public class Vector
-  extends FirstClassObject
+    extends FirstClassObject
 {
-  // TODO It is not clear how the constantness is handled for a vector.
+    // TODO It is not clear how the constantness is handled for a vector.
 
-  /**
-   * The name of the type as used by error reporting.
-   *
-   * @see FirstClassObject#getTypename()
-   */
-  public static final String TYPE_NAME = "vector";
+    /**
+     * The name of the type as used by error reporting.
+     *
+     * @see FirstClassObject#getTypename()
+     */
+    public static final String TYPE_NAME = "vector";
 
+    /**
+     * The java array that holds the vector elements.
+     */
+    private FirstClassObject[] _theArray;
 
-
-  /**
-   * The java array that holds the vector elements.
-   */
-  private FirstClassObject[] _theArray;
-
-
-
-  /**
-   * Create an array of the specified size.  The array slots are initialized
-   * to NIL (the empty list).
-   *
-   * @param size Size of the array to be created.
-   */
-  public Vector( long size )
-  {
-    this( size, Cons.NIL );
-  }
-
-
-
-  /**
-   * Creates an array of the specified size and initializes its slots
-   * to the specified value.  Note that each slot is set to the same
-   * initial object, so that the elements are equal in the sense of
-   * the scheme predicate eqv?.
-   *
-   * @param size The size of the array to be created.
-   * @param initialiser The initial value for the array slots.
-   */
-  public Vector( long size, FirstClassObject initialiser )
-  {
-    // Create the array...
-    _theArray = new FirstClassObject[ (int)size ];
-    // ...and initialize it.
-    fill( initialiser );
-  }
-
-
-
-  /**
-   * Create a Scheme vector from the passed array.  The array is copied.
-   *
-   * @param array The array holding the initial contents of the resulting
-   *              vector.
-   * @param copyArray In case this is false, the passed array won't be copied
-   *                  but used as is for the internal representation of the
-   *                  vector.
-   */
-  public Vector( FirstClassObject[] array, boolean copyArray )
-  {
-    if ( copyArray )
+    /**
+     * Create an array of the specified size.  The array slots are initialized
+     * to NIL (the empty list).
+     *
+     * @param size Size of the array to be created.
+     */
+    public Vector( long size )
     {
-      _theArray = new FirstClassObject[ array.length ];
-      System.arraycopy( array, 0, _theArray, 0, _theArray.length );
-    }
-    else
-      _theArray = array;
-  }
-
-
-
-  /**
-   * Create a Scheme vector from the passed array.  The array is copied.
-   *
-   * @param array The array holding the initial contents of the resulting
-   *              vector.
-   */
-  public Vector( FirstClassObject[] array )
-  {
-    this( array, true );
-  }
-
-
-
-  /**
-   * Sets an array slot to the specified value.
-   *
-   * @param idx The index of the slot to set.
-   * @param so The value to set.
-   * @throws RuntimeX in case the array index is out of bounds.
-   */
-  public void setElement( int idx, FirstClassObject so )
-    throws RuntimeX
-  {
-    try
-    {
-      _theArray[ idx ] = so;
-    }
-    catch ( ArrayIndexOutOfBoundsException e )
-    {
-      throw new RuntimeX( "INDEX_OUT_OF_BOUNDS", new Object[]{ "" + idx } );
-    }
-  }
-
-
-
-  /**
-   * Access the value of an array slot.
-   *
-   * @param idx The index of the slot to access.
-   * @return The slot's value.
-   */
-  public FirstClassObject getElement( int idx )
-  {
-    return _theArray[ idx ];
-  }
-
-
-
-  /**
-   * Get the size of the array.
-   *
-   * @return The size of the array.
-   */
-  public long getSize()
-  {
-    return _theArray.length;
-  }
-
-
-
-  /**
-   * Fill all vector slots with the passed value.  The value is not copied.
-   *
-   * @param filler The value to be used for filling the vector.
-   */
-  public void fill( FirstClassObject filler )
-  {
-    Arrays.fill( _theArray, filler );
-  }
-
-
-
-  /**
-   * The implementation of the scheme equal? procedure.  This is the least
-   * efficient one since lists and arrays are deep compared. For other types
-   * eqv? is used.
-   *
-   * @param other The FirstClassObject to compare with.
-   */
-  public boolean equal( FirstClassObject other )
-  {
-    if ( other instanceof Vector &&
-         _theArray.length == ((Vector)other)._theArray.length )
-    {
-      Vector otherVector = (Vector)other;
-
-      for ( int i = 0 ; i < _theArray.length ; i++ )
-      {
-        boolean idxeqv =
-          equal( _theArray[i], otherVector._theArray[i] );
-
-        if ( ! idxeqv )
-          return idxeqv;
-      }
-
-      return true;
+        this( size, Cons.NIL );
     }
 
-    return false;
-  }
-
-
-
-  /**
-   * @see FirstClassObject#toString
-   */
-  public String toString()
-  {
-    StringBuilder result = new StringBuilder( "#(" );
-
-    // Add one vector element after the other.
-    for ( int i = 0 ; i < _theArray.length ; i++ )
+    /**
+     * Creates an array of the specified size and initializes its slots
+     * to the specified value.  Note that each slot is set to the same
+     * initial object, so that the elements are equal in the sense of
+     * the scheme predicate eqv?.
+     *
+     * @param size The size of the array to be created.
+     * @param initialiser The initial value for the array slots.
+     */
+    public Vector( long size, FirstClassObject initialiser )
     {
-      result.append( stringize( _theArray[i] ) );
-      result.append( ' ' );
+        _theArray = new FirstClassObject[ (int)size ];
+        fill( initialiser );
     }
 
-    // Remove the space at the end added in the above loop.
-    if ( _theArray.length > 0 )
-      result.setLength( result.length() -1 );
+    /**
+     * Create a Scheme vector from the passed array.  The array is copied.
+     *
+     * @param array The array holding the initial contents of the resulting
+     *              vector.
+     * @param copyArray In case this is false, the passed array won't be copied
+     *                  but used as is for the internal representation of the
+     *                  vector.
+     */
+    public Vector( FirstClassObject[] array, boolean copyArray )
+    {
+        if ( copyArray )
+        {
+            _theArray = Arrays.copyOf( array, array.length);
+        }
+        else
+            _theArray = array;
+    }
 
-    return result.append( ')' ).toString();
-  }
+    /**
+     * Create a Scheme vector from the passed array.  The array is copied.
+     *
+     * @param array The array holding the initial contents of the resulting
+     *              vector.
+     */
+    public Vector( FirstClassObject[] array )
+    {
+        this( array, true );
+    }
 
+    /**
+     * Sets an array slot to the specified value.
+     *
+     * @param idx The index of the slot to set.
+     * @param so The value to set.
+     * @throws RuntimeX in case the array index is out of bounds.
+     */
+    public void set( long idx, FirstClassObject so )
+            throws RuntimeX
+    {
+        try
+        {
+            _theArray[ (int)idx ] = so;
+        }
+        catch ( ArrayIndexOutOfBoundsException e )
+        {
+            throw new RuntimeX( "INDEX_OUT_OF_BOUNDS", new Object[]{ "" + idx } );
+        }
+    }
 
+    /**
+     * Access the value of an array slot.
+     *
+     * @param idx The index of the slot to access.
+     * @return The slot's value.
+     */
+    public FirstClassObject get( long idx )
+        throws RuntimeX
+    {
+        try
+        {
+            return _theArray[ (int)idx ];
+        }
+        catch ( IndexOutOfBoundsException e )
+        {
+            throw new RuntimeX( Code.INDEX_OUT_OF_BOUNDS, "" + idx );
+        }
+    }
 
-  /**
-   * @see FirstClassObject#clone
-   */
-  public Object clone()
-  {
-    return new Vector( _theArray );
-  }
+    /**
+     * Get the size of the array.
+     *
+     * @return The size of the array.
+     */
+    public long size()
+    {
+        return _theArray.length;
+    }
 
+    /**
+     * Fill all vector slots with the passed value.  The value is not copied.
+     *
+     * @param filler The value to be used for filling the vector.
+     */
+    public void fill( FirstClassObject filler )
+    {
+        Arrays.fill( _theArray, filler );
+    }
 
+    /**
+     * The implementation of the scheme equal? procedure.  This is the least
+     * efficient one since lists and arrays are deep compared. For other types
+     * eqv? is used.
+     *
+     * @param other The FirstClassObject to compare with.
+     */
+    @Override
+    public boolean equal( FirstClassObject other )
+    {
+        if ( other instanceof Vector &&
+                _theArray.length == ((Vector)other)._theArray.length )
+        {
+            Vector otherVector = (Vector)other;
 
-  /**
-   * Converts the vector to an array of <code>java.lang.Object</code>s holding
-   * recursively converted entries.
-   */
-  public Object convertToJava()
-  {
-    Object[] result = new Object[ _theArray.length ];
+            for ( int i = 0 ; i < _theArray.length ; i++ )
+            {
+                boolean idxeqv =
+                        equal( _theArray[i], otherVector._theArray[i] );
 
-    for ( int i = 0 ; i < result.length ; i++ )
-      result[i] = _theArray[i].convertToJava();
+                if ( ! idxeqv )
+                    return idxeqv;
+            }
 
-    return result;
-  }
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @see FirstClassObject#toString
+     */
+    @Override
+    public String toString()
+    {
+        StringBuilder result = new StringBuilder( "#(" );
+
+        // Add one vector element after the other.
+        for ( int i = 0 ; i < _theArray.length ; i++ )
+        {
+            result.append( stringize( _theArray[i] ) );
+            result.append( ' ' );
+        }
+
+        // Remove the space at the end added in the above loop.
+        if ( _theArray.length > 0 )
+            result.setLength( result.length() -1 );
+
+        return result.append( ')' ).toString();
+    }
+
+    /**
+     * @see FirstClassObject#clone
+     */
+    @Override
+    public Object clone()
+    {
+        return new Vector( _theArray );
+    }
+
+    /**
+     * Converts the vector to an array of <code>java.lang.Object</code>s holding
+     * recursively converted entries.
+     */
+    @Override
+    public Object convertToJava()
+    {
+        Object[] result = new Object[ _theArray.length ];
+
+        for ( int i = 0 ; i < result.length ; i++ )
+            result[i] = _theArray[i].convertToJava();
+
+        return result;
+    }
 }
--- src/test/java/de/michab/scream/VectorTest.java
+++ src/test/java/de/michab/scream/VectorTest.java
@@ -1,6 +1,7 @@
 package de.michab.scream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,12 +13,86 @@ public class VectorTest
         var size = 5;
         Vector v = new Vector( size );
 
-        assertEquals( size, v.getSize() );
+        assertEquals( size, v.size() );
         for ( int i = 0 ; i < size ; i++ )
-            assertEquals( Cons.NIL, v.getElement( i ) );
+            assertEquals( Cons.NIL, v.get( i ) );
 
         v.fill( TestUtil.s1 );
         for ( int i = 0 ; i < size ; i++ )
-            assertEquals( TestUtil.s1, v.getElement( i ) );
+            assertEquals( TestUtil.s1, v.get( i ) );
+    }
+
+    @Test
+    public void copyYes() throws Exception
+    {
+        var a = new FirstClassObject[]
+        {
+            TestUtil.i1,
+            TestUtil.i2
+        };
+
+        var vCopy = new Vector( a );
+        assertEquals( a.length, vCopy.size() );
+        vCopy.set( 0, TestUtil.s313 );
+        assertEquals( TestUtil.s313, vCopy.get( 0 ) );
+    }
+
+    @Test
+    public void copyNo() throws Exception
+    {
+        var a = new FirstClassObject[]
+        {
+            TestUtil.i1,
+            TestUtil.i2
+        };
+
+        var vCopy = new Vector( a, false );
+        assertEquals( a.length, vCopy.size() );
+        vCopy.set( 0, TestUtil.s313 );
+        assertEquals( TestUtil.s313, vCopy.get( 0 ) );
+        assertEquals( TestUtil.s313, a[0] );
+    }
+
+    @Test
+    public void vector_length_Test() throws Exception
+    {
+        SchemeInterpreter2 si = new SchemeInterpreter2();
+        var se = si.getScriptEngine();
+
+        var result = se.eval(
+                """
+                (define v #(1 2 3))
+                (vector-length v)
+                """ );
+        assertEquals( result, TestUtil.i3 );
+    }
+
+    @Test
+    public void vector_ref_Test() throws Exception
+    {
+        SchemeInterpreter2 si = new SchemeInterpreter2();
+        var se = si.getScriptEngine();
+
+        var result = (FirstClassObject)se.eval(
+                """
+                (define v #(1 2 3))
+                (vector-ref v 0)
+                """ );
+        assertTrue( TestUtil.i1.equal( result ) );
+    }
+
+    @Test
+    public void vector_set_Test() throws Exception
+    {
+        SchemeInterpreter2 si = new SchemeInterpreter2();
+        var se = si.getScriptEngine();
+
+        var result = (FirstClassObject)se.eval(
+                """
+                (define v #(1 2 3))
+                (vector-set! v 0 313)
+                (vector-ref v 0)
+                """ );
+        assertTrue( TestUtil.i313.equal( result ) );
     }
 }
