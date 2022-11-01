60de6229e022b6d64b2f3f10093fbf4f2e7df417
Michael Binz <michab66@gmail.com>
2022-11-01 21:32:25 +0100

    Tests.

--- src/test/java/de/michab/scream/util/ScutTest.java
+++ src/test/java/de/michab/scream/util/ScutTest.java
@@ -3,9 +3,12 @@ package de.michab.scream.util;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.HashSet;
+
 import org.junit.jupiter.api.Test;
 
 import de.michab.scream.Cons;
+import de.michab.scream.FirstClassObject;
 import de.michab.scream.RuntimeX;
 import de.michab.scream.ScreamBaseTest;
 import de.michab.scream.ScreamException.Code;
@@ -50,6 +53,27 @@ public class ScutTest extends ScreamBaseTest
         try
         {
             Scut.checkUnique( list );
+            fail();
+        }
+        catch ( RuntimeX x )
+        {
+            assertEquals( Code.DUPLICATE_ELEMENT, x.getCode() );
+            assertEquals( i1.toString(), x.getArguments()[0] );
+        }
+    }
+
+    @Test
+    public void notUniqueMultiStep() throws Exception
+    {
+        HashSet<FirstClassObject> unifier = new HashSet<>();
+
+        var l1 = readSingleExpression( "(1 2 3)", Cons.class );
+        var l2 = readSingleExpression( "(4 5 1)", Cons.class );
+
+        try
+        {
+            Scut.checkUnique( unifier, l1 );
+            Scut.checkUnique( unifier, l2 );
             fail();
         }
         catch ( RuntimeX x )
