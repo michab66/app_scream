fd4ef6061d03bc58d83029f69674f3ab422f1bc5
Michael Binz <michab66@gmail.com>
2022-11-03 20:46:50 +0100

    Syntax if move.

--- src/main/java/de/michab/scream/Syntax.java
+++ src/main/java/de/michab/scream/Syntax.java
@@ -9,7 +9,6 @@ package de.michab.scream;
 import de.michab.scream.Lambda.L;
 import de.michab.scream.ScreamException.Code;
 import de.michab.scream.pops.Assignment;
-import de.michab.scream.pops.If;
 import de.michab.scream.pops.Let;
 import de.michab.scream.pops.LetAsterisk;
 import de.michab.scream.pops.Letrec;
@@ -198,122 +197,6 @@ public class Syntax
         return evaluateTrailingContext( seq[ seq.length-1 ], env );
     }
 
-    /**
-     * r7rs 4.1.5
-     *
-     * <code>
-     * (if <test> <consequent> <alternate>)<br>
-     * (if <test> <consequent>)<br>
-     * </code><br>
-     * Syntax: <Test>, <consequent>, and <alternate> may be arbitrary
-     * expressions.<br>
-     * Semantics: An if expression is evaluated as follows:  First, <test> is
-     * evaluated. If it yields a true value, then
-     * <consequent> is evaluated and its value(s) is(are) returned. Otherwise
-     * <alternate> is evaluated and its value(s) is(are) returned. If <test>
-     * yields a false value and no <alternate> is specified, then the result of
-     * the expression is unspecified.
-     */
-    static private Operation ifSyntax = new Syntax( "if" )
-    {
-        private Lambda compImpl( Environment env, Cons cond, Cons positive )
-                throws RuntimeX
-        {
-            var ccond = cond.getCar().compile( env );
-            cond.setCar( ccond );
-            var cpositive = positive.getCar().compile( env );
-            positive.setCar( cpositive );
-
-            Lambda.L result = (e,c) -> {
-                return Continuation._if(
-                        e,
-                        ccond,
-                        cpositive,
-                        c );
-            };
-
-            return new Lambda( result, getName() );
-        }
-        private Lambda compImpl( Environment env, Cons cond, Cons positive, Cons negative )
-                throws RuntimeX
-        {
-            var ccond = cond.getCar().compile( env );
-            cond.setCar( ccond );
-            var cpositive = positive.getCar().compile( env );
-            positive.setCar( cpositive );
-            var cnegative = negative.getCar().compile( env );
-            negative.setCar( cnegative );
-
-            Lambda.L result = (e,c) -> {
-                return Continuation._if(
-                        e,
-                        ccond,
-                        cpositive,
-                        cnegative,
-                        c );
-            };
-
-            return new Lambda( result, getName() );
-        }
-
-        @Override
-        protected Lambda _compile( Environment env, Cons args ) throws RuntimeX
-        {
-            long argsLen =
-                    checkArgumentCount( 2, 3, args );
-
-            if ( argsLen == 2 )
-            {
-                return compImpl(
-                        env,
-                        args,
-                        Scut.as( Cons.class, args.listTail( 1 ) ) );
-            }
-            else if ( argsLen == 3 )
-            {
-                return compImpl(
-                        env,
-                        args,
-                        Scut.as( Cons.class, args.listTail( 1 ) ),
-                        Scut.as( Cons.class, args.listTail( 2 ) ) );
-            }
-
-            throw new InternalError();
-        }
-
-        // Legacy ..
-        @Override
-        public FirstClassObject compile( Environment parent, FirstClassObject[] args )
-                throws RuntimeX
-        {
-            checkMinimumArgumentCount( 2, args );
-            checkMaximumArgumentCount( 3, args );
-
-            // Compile referenced nodes.
-            for ( int i = 0 ; i < args.length ; i++ )
-                args[i] = compile( args[i], parent );
-
-            FirstClassObject condition = args[0];
-            FirstClassObject onTrue = args[1];
-            // Handle optional 'else' branch.
-            FirstClassObject onFalse = args.length == 3 ? args[2] : Cons.NIL;
-
-            // Optimisation of constant sub expressions.  If this is sth like
-            // (if #t ...) no 'if' node is needed at all.
-            //      if ( isConstant( condition ) )
-            //      {
-            //        System.err.println( "removed 'if'" );
-            //
-            //        if ( condition != SchemeBoolean.F )
-            //          return onTrue;
-            //        else
-            //          return onFalse;
-            //      }
-
-            // Now create the compiled node.
-            return new If( condition, onTrue, onFalse );
-        }
-    };
 
     /**
      * (and <test1> ...) syntax; r5rs 11
@@ -832,7 +715,6 @@ public class Syntax
      */
     static Environment extendTopLevelEnvironment( Environment tle )
     {
-        tle.setPrimitive( ifSyntax );
         tle.setPrimitive( andSyntax );
         tle.setPrimitive( orSyntax );
         tle.setPrimitive( letSyntax );
