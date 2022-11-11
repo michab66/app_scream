832871b7b66cb3df1875c6dbfd4bd05e37b459e3
Michael Binz <michab66@gmail.com>
2022-11-11 15:15:18 +0100

    Syntax let tests.

---
+++ src/test/java/de/michab/scream/language/LetStarTest.java
@@ -0,0 +1,46 @@
+/*
+ * Scream @ https://github.com/michab/dev_smack
+ *
+ * Copyright © 1998-2022 Michael G. Binz
+ */
+package de.michab.scream.language;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import de.michab.scream.ScreamBaseTest;
+
+/**
+ * r7rs 4.2.2 Binding constructs, p16
+ */
+public class LetStarTest extends ScreamBaseTest
+{
+    @Test
+    public void letStar_1() throws Exception
+    {
+        // x is defined in the second initialization
+        // in difference to plain let.
+        var result = scriptEngine().evalFco(
+            """
+            (let*
+            ((x 2) (y x))
+            y)
+            """ );
+        assertEquals( i2, result );
+    }
+
+    @Test
+    public void letStar_2() throws Exception
+    {
+        // r7rs: The <variable>s need not be distinct.
+        var result = scriptEngine().evalFco(
+            """
+            (let*
+            ((x 2) (x 312) (x (+ 1 x)) (y x))
+            y)
+            """ );
+        assertEquals( i313, result );
+    }
+
+}
---
+++ src/test/java/de/michab/scream/language/LetTest.java
@@ -0,0 +1,89 @@
+/*
+ * Scream @ https://github.com/michab/dev_smack
+ *
+ * Copyright © 1998-2022 Michael G. Binz
+ */
+package de.michab.scream.language;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import de.michab.scream.RuntimeX;
+import de.michab.scream.ScreamBaseTest;
+import de.michab.scream.ScreamException.Code;
+
+/**
+ * r7rs 4.2.2 Binding constructs, p16
+ */
+public class LetTest extends ScreamBaseTest
+{
+    @Test
+    public void letTest_1() throws Exception
+    {
+        try
+        {
+            // x is not defined in second init in
+            // difference to let*.
+            scriptEngine().evalFco(
+                """
+                (let
+                ((x 2) (y x))
+                y)
+                """ );
+            fail();
+        }
+        catch ( RuntimeX rx )
+        {
+            assertEquals( Code.SYMBOL_NOT_DEFINED, rx.getCode() );
+            assertEquals( s("x"), rx.getArgument( 0 ) );
+        }
+    }
+
+    @Test
+    public void letTest_2() throws Exception
+    {
+        try
+        {
+            // Evaluation of y must fail.
+            scriptEngine().evalFco(
+                """
+                (let
+                ((x 2) (z 3))
+                y)
+                """ );
+            fail();
+        }
+        catch ( RuntimeX rx )
+        {
+            assertEquals( Code.SYMBOL_NOT_DEFINED, rx.getCode() );
+            assertEquals( s("y"), rx.getArgument( 0 ) );
+        }
+    }
+
+    @Test
+    @Disabled
+    public void letTest_3() throws Exception
+    {
+        try
+        {
+            // r7rsb: It is an error for a <variable> to
+            // appear more than once in the list of variables being bound.
+            scriptEngine().evalFco(
+                """
+                (let
+                ((x 2) (x 3))
+                8)
+                """ );
+            fail();
+        }
+        catch ( RuntimeX rx )
+        {
+            assertEquals( Code.DUPLICATE_ELEMENT, rx.getCode() );
+            assertEquals( s("x"), rx.getArgument( 0 ) );
+        }
+    }
+
+}
