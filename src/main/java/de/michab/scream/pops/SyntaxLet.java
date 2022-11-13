7fe653062bdecc5ae485ec509d49ad289623356e
Michael Binz <michab66@gmail.com>
2022-11-13 12:09:02 +0100

    Dead wood.

--- src/main/java/de/michab/scream/pops/SyntaxLet.java
+++ src/main/java/de/michab/scream/pops/SyntaxLet.java
@@ -1,3 +1,8 @@
+/*
+ * Scream @ https://github.com/michab/dev_smack
+ *
+ * Copyright © 1998-2022 Michael G. Binz
+ */
 package de.michab.scream.pops;
 
 import de.michab.scream.Cons;
@@ -6,7 +11,6 @@ import de.michab.scream.FirstClassObject;
 import de.michab.scream.Lambda;
 import de.michab.scream.Lambda.L;
 import de.michab.scream.RuntimeX;
-import de.michab.scream.ScreamException.Code;
 import de.michab.scream.Symbol;
 import de.michab.scream.Syntax;
 import de.michab.scream.util.Scut;
@@ -78,83 +82,20 @@ public abstract class SyntaxLet
     }
 
     @Override
-    public FirstClassObject compile( Environment parent, FirstClassObject[] args )
+    public FirstClassObject compile( Environment parent, Cons args )
             throws RuntimeX
     {
-        checkMinimumArgumentCount( 2, args );
-
-        FirstClassObject exceptionInfo = Cons.NIL;
-
-        Symbol[] variables = null;
-        FirstClassObject[] inits = null;
-
-        // If there are bindings...
-        if ( args[0] != Cons.NIL )
-        {
-            // ...decompose them.  As soon something is wrong a ClassCastException
-            // is thrown, resulting in an error message.
-            try
-            {
-                exceptionInfo = args[0];
-                // CCEx here if bindings was no Cons.
-                FirstClassObject[] bindings = ((Cons)args[0]).asArray();
-                variables = new Symbol[ bindings.length ];
-                inits = new FirstClassObject[ bindings.length ];
-
-                for ( int i = 0 ; i < bindings.length ; i++ )
-                {
-                    exceptionInfo = bindings[i];
-                    // CCEx here if a single binding was no cons.
-                    Cons binding = (Cons)bindings[i];
-                    if ( Cons.NIL == binding )
-                        throw new ClassCastException();
-                    // Check whether the single binding is a proper two element list
-                    // and throw an artificial CCEx to get in the BAD_BINDING handler.
-                    if ( !binding.isProperList() || binding.length() != 2 )
-                        throw new ClassCastException();
-                    // Decompose the single binding.  CCEx here if the first element in
-                    // the list is no symbol.
-                    variables[i] = (Symbol)binding.listRef( 0 );
-                    // CCEx here is not really possible.
-                    inits[i] = compile( binding.listRef( 1 ), parent );
-                }
-            }
-            catch ( ClassCastException e )
-            {
-                throw new RuntimeX(
-                        Code.BAD_BINDING,
-                        toString( getName() ),
-                        toString( exceptionInfo ) );
-            }
-        }
-        else
-        {
-            // We received no bindings.
-            variables = new Symbol[0];
-            inits = new FirstClassObject[0];
-        }
-
-        FirstClassObject[] body = new FirstClassObject[ args.length - 1 ];
-        System.arraycopy( args, 1, body, 0, body.length );
-        for ( int i = body.length-1 ; i >= 0 ; i-- )
-            body[i] = compile( body[i], parent );
-
-        return createPop( variables, inits, body );
+        return _compile( parent, args );
     }
+    @Override
+    public FirstClassObject activate( Environment parent,
+            Cons arguments )
+                    throws RuntimeX
+    {
+        var λ = _compile( parent, arguments );
 
-    /**
-     * Create the actual primitive operation that implements the let syntax.
-     * This is a template method to be overridden by the concrete let
-     * implementations.
-     *
-     * @param variables
-     * @param inits
-     * @param body
-     * @return The newly created primitive.
-     */
-    abstract FirstClassObject createPop( Symbol[] variables,
-            FirstClassObject[] inits,
-            FirstClassObject[] body );
+        return FirstClassObject.evaluate( λ, parent );
+    }
 
     /**
      * (let <bindings> <body>) syntax r7rs, p16
@@ -183,31 +124,6 @@ public abstract class SyntaxLet
 
             return new Lambda( l, getName() );
         }
-
-        @Override
-        FirstClassObject createPop( Symbol[] variables,
-                FirstClassObject[] inits,
-                FirstClassObject[] body )
-        {
-            throw new InternalError();
-        }
-
-        @Override
-        public FirstClassObject compile( Environment parent, Cons args )
-                throws RuntimeX
-        {
-            return _compile( parent, args );
-        }
-        @Override
-        public FirstClassObject activate( Environment parent,
-                Cons arguments )
-                        throws RuntimeX
-        {
-            var λ = _compile( parent, arguments );
-
-            return FirstClassObject.evaluate( λ, parent );
-        }
-
     };
 
     /**
@@ -236,30 +152,6 @@ public abstract class SyntaxLet
                     c);
 
             return new Lambda( l, getName() );
-        }
-
-        @Override
-        FirstClassObject createPop( Symbol[] variables,
-                FirstClassObject[] inits,
-                FirstClassObject[] body )
-        {
-            throw new InternalError();
-        }
-
-        @Override
-        public FirstClassObject compile( Environment parent, Cons args )
-                throws RuntimeX
-        {
-            return _compile( parent, args );
-        }
-        @Override
-        public FirstClassObject activate( Environment parent,
-                Cons arguments )
-                        throws RuntimeX
-        {
-            var λ = _compile( parent, arguments );
-
-            return FirstClassObject.evaluate( λ, parent );
         }
     };
 
@@ -290,30 +182,6 @@ public abstract class SyntaxLet
                     c);
 
             return new Lambda( l, getName() );
-        }
-
-        @Override
-        public FirstClassObject compile( Environment parent, Cons args )
-                throws RuntimeX
-        {
-            return _compile( parent, args );
-        }
-        @Override
-        public FirstClassObject activate( Environment parent,
-                Cons arguments )
-                        throws RuntimeX
-        {
-            var λ = _compile( parent, arguments );
-
-            return FirstClassObject.evaluate( λ, parent );
-        }
-
-        @Override
-        FirstClassObject createPop( Symbol[] variables,
-                FirstClassObject[] inits,
-                FirstClassObject[] body )
-        {
-            throw new InternalError();
         }
     };
 
