990a7bf14fb7ea8cfc52a948ab93bf7841e46538
Michael Binz <michab66@gmail.com>
2022-11-13 12:42:06 +0100

    Added test.

--- src/test/java/de/michab/scream/language/R7rs_4_2_4_Iteration_Test.java
+++ src/test/java/de/michab/scream/language/R7rs_4_2_4_Iteration_Test.java
@@ -35,6 +35,24 @@ public class R7rs_4_2_4_Iteration_Test extends ScreamBaseTest
      * p18
      */
     @Test
+    public void let_1x() throws Exception
+    {
+        var result = scriptEngine().evalFco(
+            """
+            (do ((vec (make-vector 7))
+                 (i 0 (+ i 1)))
+                ((= i (vector-length vec)) vec)
+              (vector-set! vec i i))
+            """ );
+        assertEqualq(
+                parse("#(0 1 2 3 4 5 6)"),
+                result );
+    }
+
+    /**
+     * p18
+     */
+    @Test
     public void let_2() throws Exception
     {
         var result = scriptEngine().evalFco(
