36f1156f0f09a5ac5d3996b456f1f20836f82373
Michael Binz <michab66@gmail.com>
2022-11-22 20:21:38 +0100

    Implemented. (#47)

---
+++ src/main/resources/de/michab/scream/extensions/common-init.s
@@ -0,0 +1,19 @@
+;
+; Scream @ https://github.com/michab/dev_smack
+;
+; Copyright © 1998-2022 Michael G. Binz
+;
+
+; Perform common runtime initialisation.
+(load "basic.s") 
+(load "quasiquote.s")
+(load "symbol.s")
+(load "boolean.s")
+(load "math.s")
+(load "cons.s")
+(load "vector.s")
+(load "char.s")
+(load "string.s")
+(load "procedure.s")
+(load "object.s")
+(load "cleanup.s")
