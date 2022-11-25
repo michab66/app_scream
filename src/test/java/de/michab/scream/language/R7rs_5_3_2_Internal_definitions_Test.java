a8aaeaa54e0526ce39d99ab6416e153ecb94209a
Michael Binz <michab66@gmail.com>
2022-11-25 16:46:17 +0100

    Added tests.

---
+++ src/test/java/de/michab/scream/language/R7rs_5_3_2_Internal_definitions_Test.java
@@ -0,0 +1,32 @@
+package de.michab.scream.language;
+
+import org.junit.jupiter.api.Test;
+
+import de.michab.scream.ScreamBaseTest;
+
+/**
+ * r7rs 5.3.2 Internal definitions
+ *
+ * @author MICBINZ
+ */
+public class R7rs_5_3_2_Internal_definitions_Test extends ScreamBaseTest
+{
+    /**
+     * p26
+     */
+    @Test
+    public void internal_definitions_1() throws Exception
+    {
+        var result = scriptEngine().evalFco(
+"""
+(let ((x 5))
+ (define foo (lambda (y) (bar x y)))
+ (define bar (lambda (a b) (+ (* a b) a)))
+ (foo (+ x 3)))
+""" );
+        assertEqualq(
+                i(45),
+                result );
+    }
+
+}
