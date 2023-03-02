322c6bd8637285dc702fffe19df8156cd7b0d864
Michael Binz <michab66@gmail.com>
2023-03-02 22:34:04 +0100

    Fixed formals parsing.

--- src/main/java/de/michab/scream/pops/SyntaxLambda.java
+++ src/main/java/de/michab/scream/pops/SyntaxLambda.java
@@ -11,10 +11,9 @@ import de.michab.scream.fcos.Cons;
 import de.michab.scream.fcos.Environment;
 import de.michab.scream.fcos.FirstClassObject;
 import de.michab.scream.fcos.Procedure;
-import de.michab.scream.fcos.Symbol;
 import de.michab.scream.fcos.Syntax;
-import de.michab.scream.util.Scut;
 import de.michab.scream.util.Continuation.Thunk;
+import de.michab.scream.util.Scut;
 
 public final class SyntaxLambda extends Syntax
 {
@@ -29,7 +28,7 @@ public final class SyntaxLambda extends Syntax
     {
         checkArgumentCount( 2, Integer.MAX_VALUE, args );
         var formals = args.listRef( 0 );
-        checkArgument( 0, formals, Symbol.class, Cons.class );
+        checkFormals( formals );
         var body = Scut.as( Cons.class,args.getCdr() );
 
         return c.accept(
--- src/test/java/de/michab/scream/FirstClassObjectTest.java
+++ src/test/java/de/michab/scream/FirstClassObjectTest.java
@@ -6,11 +6,15 @@
 package de.michab.scream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import de.michab.scream.fcos.Cons;
 import de.michab.scream.fcos.FirstClassObject;
 import de.michab.scream.fcos.SchemeInteger;
+import de.michab.scream.fcos.Symbol;
 import de.michab.scream.util.Scut;
 
 public class FirstClassObjectTest extends ScreamBaseTest
@@ -26,5 +30,14 @@ public class FirstClassObjectTest extends ScreamBaseTest
 
         SchemeInteger i = Scut.as( SchemeInteger.class, fco );
         assertEquals( 313L, i.asLong() );
+    }
+
+    public void is() throws Exception
+    {
+        assertTrue( FirstClassObject.is( Cons.class, Cons.NIL ) );
+        assertTrue( FirstClassObject.is( Symbol.class, s("gaukeley") ) );
+
+        assertFalse( FirstClassObject.is( Symbol.class, Cons.NIL ) );
+        assertFalse( FirstClassObject.is( Symbol.class, i(313) ) );
     }
 }
