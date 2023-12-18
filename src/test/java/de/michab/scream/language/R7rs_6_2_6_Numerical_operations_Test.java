6a357d3ccb18e55db60df7cc297119fb0fe6818d
Michael Binz <michab66@gmail.com>
2023-12-18 21:09:42 +0100

    224 operation sqrt not working (#229)
    * Fixed.
    
    * Added missing file.
    
    * Fixed naming.
    
    * Fixed test.

---
+++ src/test/java/de/michab/scream/language/R7rs_6_2_6_Numerical_operations_Test.java
@@ -0,0 +1,39 @@
+/*
+ * Scream @ https://github.com/urschleim/scream
+ *
+ * Copyright © 1998-2023 Michael G. Binz
+ */
+package de.michab.scream.language;
+
+import org.junit.jupiter.api.Test;
+
+import de.michab.scream.ScreamBaseTest;
+
+public class R7rs_6_2_6_Numerical_operations_Test extends ScreamBaseTest
+{
+    @Test
+    public void sqrt() throws Exception
+    {
+        final var SQRT2 = Math.sqrt( 2.0 );
+
+        expectFco(
+                "(sqrt 2)",
+                d( SQRT2 ) );
+    }
+    @Test
+    public void sqrt_2() throws Exception
+    {
+        final var SQRT2 = Math.sqrt( 2.0 );
+
+        expectFco(
+                "(sqrt 2.0)",
+                d( SQRT2 ) );
+    }
+    @Test
+    public void sqrt_3() throws Exception
+    {
+        expectFco(
+                "(integer? (sqrt 4))",
+                bTrue );
+    }
+}
