b38dc18d95b70b6f0fb477058c47c6ec3fd05806
Michael Binz <michab66@gmail.com>
2023-12-20 21:51:19 +0100

    Reformat test.

--- src/test/java/de/michab/scream/language/R7rs_4_2_4_Iteration_Test.java
+++ src/test/java/de/michab/scream/language/R7rs_4_2_4_Iteration_Test.java
@@ -39,13 +39,13 @@ public class R7rs_4_2_4_Iteration_Test extends ScreamBaseTest
     public void do_1x() throws Exception
     {
         expectFco(
-            """
-            (do ((vec (make-vector 7))
-                 (i 0 (+ i 1)))
-                ((= i (vector-length vec)) vec)
-              (vector-set! vec i i))
-            """,
-            "#(0 1 2 3 4 5 6)" );
+"""
+        (do ((vec (make-vector 7))
+             (i 0 (+ i 1)))
+            ((= i (vector-length vec)) vec)
+          (vector-set! vec i i))
+""",
+        "#(0 1 2 3 4 5 6)" );
     }
 
     /**
@@ -55,33 +55,30 @@ public class R7rs_4_2_4_Iteration_Test extends ScreamBaseTest
     public void do_2() throws Exception
     {
         expectFco(
-            """
-            (let ((x '(1 3 5 7 9)))
-              (do ((x x (cdr x))
-                   (sum 0 (+ sum (car x))))
-                  ((null? x) sum)))
-            """,
-            i(25) );
+"""
+        (let ((x '(1 3 5 7 9)))
+          (do ((x x (cdr x))
+               (sum 0 (+ sum (car x))))
+              ((null? x) sum)))
+""",
+        i(25) );
     }
 
     /**
-     * #186
+     * Issue #186 : https://github.com/urschleim/scream/issues/186
      */
     @Test
     public void do_186() throws Exception
     {
-        var se = scriptEngine();
-
         expectFco(
-            se,
-            """
-(define (range-test b e)
-  (do ((r '() (cons e r))
-        (e (- e 1) (- e 1)))
-       ((< e b) r)))
-(range-test 3 5)
-            """,
-            parse( "(3 4)" ) );
+"""
+        (define (range-test b e)
+          (do ((r '() (cons e r))
+                (e (- e 1) (- e 1)))
+               ((< e b) r)))
+        (range-test 3 5)
+""",
+        "(3 4)" );
     }
 
     /**
