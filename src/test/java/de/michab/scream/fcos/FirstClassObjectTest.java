ba0813921b16c5f6f2b0fc2f9e6068b0bb39e967
Michael Binz <michab66@gmail.com>
2023-12-22 12:07:04 +0100

    Fixed.

--- src/test/java/de/michab/scream/fcos/FirstClassObjectTest.java
+++ src/test/java/de/michab/scream/fcos/FirstClassObjectTest.java
@@ -5,8 +5,8 @@
  */
 package de.michab.scream.fcos;
 
-import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
