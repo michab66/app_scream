80350614950d3f482d6bf8bb9aae82156b012954
Michael Binz <michab66@gmail.com>
2024-01-03 11:15:30 +0100

    Cleanup, test enabling. (#255)

---
+++ src/test/java/de/michab/scream/language/R7rs_2_2_Whitespace_and_comments.java
@@ -0,0 +1,63 @@
+/*
+ * Scream @ https://github.com/urschleim/scream
+ *
+ * Copyright © 2024 Michael G. Binz
+ */
+package de.michab.scream.language;
+
+import org.junit.jupiter.api.Test;
+
+import de.michab.scream.ScreamBaseTest;
+
+public class R7rs_2_2_Whitespace_and_comments extends ScreamBaseTest
+{
+    /**
+     * r7rs 2.2 p8
+     */
+    @Test
+    public void r7rs_example() throws Exception
+    {
+        expectFco(
+                """
+                #|
+                    The FACT procedure computes the factorial
+                    of a non-negative integer.
+                |#
+                (define fact
+                  (lambda (n)
+                    (if (= n 0)
+                      #; (= n 1)
+                      1  ; Base case: return 1
+                      (* n (fact (- n 1))))))
+
+                (fact 19)
+
+                """,
+
+                "121645100408832000" );
+    }
+
+    @Test
+    public void nested_substructure() throws Exception
+    {
+        expectFco(
+                """
+                #| Prefix.  Nesting =>  #|
+                    The FACT procedure computes the factorial
+                    of a non-negative integer.
+                |# <= end nesting.
+                |#
+                (define fact
+                  #;(lambda (n) ; <= commented.
+                    (if (= n 0)
+                      #; (= n 1) ; <= nested.
+                      1  ; Base case: return 1
+                      (* n (fact (- n 1))))) 313)
+
+                fact
+
+                """,
+
+                i313 );
+    }
+}
--- src/test/java/de/michab/scream/language/R7rs_4_2_3_SequencingTest.java
+++ src/test/java/de/michab/scream/language/R7rs_4_2_3_SequencingTest.java
@@ -1,29 +1,66 @@
 /*
  * Scream @ https://github.com/urschleim/scream
  *
- * Copyright © 1998-2023 Michael G. Binz
+ * Copyright © 1998-2024 Michael G. Binz
  */
 package de.michab.scream.language;
-
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import org.junit.jupiter.api.Test;
 
 import de.michab.scream.ScreamBaseTest;
-import de.michab.scream.ScreamEvaluator;
-import de.michab.scream.fcos.Operation;
+import de.michab.scream.fcos.Cons;
 
 public class R7rs_4_2_3_SequencingTest extends ScreamBaseTest
 {
     @Test
-    public void exists() throws Exception
+    public void beginTest_1() throws Exception
     {
-        ScreamEvaluator se = scriptEngine();
-
-        var result = se.evalFco(
+        expectFco(
                 """
-                begin
-                """ );
-        assertInstanceOf( Operation.class, result );
+                (begin
+                  (define seq '(1))
+                  (set! seq (append seq '(2)))
+                  (set! seq (append seq '(3)))
+                  seq
+                )
+                """,
+                "(1 2 3)" );
+    }
+
+    @Test
+    public void beginTest_2() throws Exception
+    {
+        expectFco(
+                """
+                (begin)
+                """,
+                Cons.NIL );
+    }
+
+    @Test
+    public void beginTest_3() throws Exception
+    {
+        expectFco(
+                """
+                (begin
+                  (define seq '(1))
+                  (set! seq (append seq '(2)))
+                  (set! seq (append seq '(3)))
+                  seq
+                )
+                """,
+                "(1 2 3)" );
+    }
+
+    @Test
+    public void chez_beginTest_4() throws Exception
+    {
+        expectFco(
+                """
+                (begin
+                  (define donald 313))
+                donald
+                """,
+                i313 );
     }
 }
